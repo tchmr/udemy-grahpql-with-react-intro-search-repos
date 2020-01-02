4c94e328b561cba4c5ef671ae3447bafbc917727
tchmr <s7k315@gmail.com>
2020-01-02 17:09:34 +0900

    apply query and variables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
 import client from './client.js'
 import { ME } from './graphql'
+import { SEARCH_REPOSITORIES } from './graphql'
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>hello graphql</div>
-      <Query query={ME}>
-        {
-          ({ loading, error, data }) => {
-            if (loading) return 'Loading...'
-            if (error) return `Error! ${error.message}`
-            return <div>{data.user.name}</div>
-          }
-        }
-      </Query>
-    </ApolloProvider>
-  );
+const VARIABLES = {
+  after: null,
+  before: null,
+  first: 5,
+  last: null,
+  query: "フロントエンドエンジニア"
 }
 
-export default App;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = VARIABLES
+  }
+  render() {
+    const { query, first, last, before, after } = this.state
+    return (
+      <ApolloProvider client={client}>
+        <Query
+         query={SEARCH_REPOSITORIES}
+         variables={{ query, first, last, before, after }}
+        >
+          {
+            ({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+
+              console.log({data})
+              return <div></div>
+            }
+          }
+        </Query>
+      </ApolloProvider>
+    )
+  }
+}
+
+export default App
--- src/graphql.js
+++ src/graphql.js
@@ -1,10 +1,37 @@
 import gql from 'graphql-tag'
 
-export const ME = gql`
-query me {
-  user(login: "iteachonudemy") {
-    id
-    name
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($after: String, $before: String, $first: Int, $last: Int, $query: String!) {
+    search(after: $after, before: $before, first: $first, last: $last, query: $query, type: REPOSITORY) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
   }
-}
+`
+
+export const ME = gql`
+  query me {
+    user(login: "iteachonudemy") {
+      id
+      name
+    }
+  }
 `
